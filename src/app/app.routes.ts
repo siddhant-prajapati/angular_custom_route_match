d79e0143f4cff1addec34d6fd57fcdebf71b457d
Siddhant Prajapati <siddhantprajapati6377@gmail.com>
2024-05-20 21:55:45 +0530

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { Routes, UrlSegment } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        matcher: (url) => {
+          if (url.length === 1 && url[0].path.match(/^@[\w]+$/gm)) {
+            return {
+                consumed: url, 
+                posParams: {
+                    username: new UrlSegment(url[0].path.slice(1), {})
+                }
+            };
+          }
+          return null;
+        },
+        component: ProfileComponent,
+      },
+];
